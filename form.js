daaee3858cf97719a9e174209b30b604f403d1ef
Ogunsola oyindamola <honeyspringworld@gmail.com>
2020-07-26 19:12:32 +0100

    intial commit

---
+++ form.js
@@ -0,0 +1,90 @@
+var currentTab = 0;
+
+function validateForm() {
+  var x,
+    y,
+    i,
+    valid = true;
+  x = document.getElementById("msform");
+  y = x[currentTab].getElementsByTagName("input");
+  // A loop that checks every input field in the current_fs:
+  for (i = 0; i < y.length; i++) {
+    // If a field is empty...
+    if (y[i].value == "") {
+      // add an "invalid" class to the field:
+      y[i].className += " invalid";
+      // and set the current valid status to false:
+      valid = false;
+    }
+  }
+
+  return valid; // return the valid status
+}
+
+$(document).ready(function () {
+  var current_fs, next_fs, previous_fs; //fieldsets
+  var opacity;
+  $(".next").click(function () {
+    if (!validateForm()) return false;
+    current_fs = $(this).parent();
+    next_fs = $(this).parent().next();
+
+    //Add Class Active
+    $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+
+    //show the next fieldset
+    next_fs.show();
+    //hide the current fieldset with style
+    current_fs.animate(
+      { opacity: 0 },
+      {
+        step: function (now) {
+          // for making fielset appear animation
+          opacity = 1 - now;
+
+          current_fs.css({
+            display: "none",
+            position: "relative",
+          });
+          next_fs.css({ opacity: opacity });
+        },
+        duration: 600,
+      }
+    );
+  });
+
+  $(".previous").click(function () {
+    current_fs = $(this).parent();
+    previous_fs = $(this).parent().prev();
+
+    //Remove class active
+    $("#progressbar li")
+      .eq($("fieldset").index(current_fs))
+      .removeClass("active");
+
+    //show the previous fieldset
+    previous_fs.show();
+
+    //hide the current fieldset with style
+    current_fs.animate(
+      { opacity: 0 },
+      {
+        step: function (now) {
+          // for making fielset appear animation
+          opacity = 1 - now;
+
+          current_fs.css({
+            display: "none",
+            position: "relative",
+          });
+          previous_fs.css({ opacity: opacity });
+        },
+        duration: 600,
+      }
+    );
+  });
+
+  $(".submit").click(function () {
+    if (!validateForm()) return false;
+  });
+});
